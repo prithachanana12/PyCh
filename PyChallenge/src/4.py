2e1a75f58ae0c6e249c07f1892c53cc82b888393
Pritha Chanana <prithachanana@gmail.com>
2015-02-23 16:11:10 -0700

    Solution 4 - needs less than 400 iterations

---
+++ PyChallenge/src/4.py
@@ -0,0 +1,30 @@
+'''
+Created on Feb 23, 2015
+
+@author: m133293
+'''
+
+import urllib
+nothing=12345
+for i in range(1,401):
+    gh=urllib.urlopen('http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing='+str(nothing)).read()
+    if any(char.isdigit() for char in gh):
+        nothing=gh.split(' ')[-1]
+    else:
+        print gh
+        print nothing
+        break
+
+nothing=int(nothing)/2
+for j in range(i,401):
+    gh=urllib.urlopen('http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing='+str(nothing)).read()
+    if any(char.isdigit() for char in gh):
+        nothing=gh.split(' ')[-1]
+    else:
+        print gh
+        print nothing
+        print j
+        break
+#print gh
+# #print urllib.urlopen('http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing='+str(nothing)).read()
+# #print gh
