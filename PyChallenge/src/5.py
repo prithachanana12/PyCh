06e0c984da86a0de4e0664e69d24da4e65f01a1e
Pritha Chanana <prithachanana@gmail.com>
2015-02-25 11:59:09 -0700

    Solution 5 - final

--- PyChallenge/src/5.py
+++ PyChallenge/src/5.py
@@ -7,3 +7,7 @@ Created on Feb 23, 2015
 import pickle,urllib
 data= pickle.load(urllib.urlopen('http://www.pythonchallenge.com/pc/def/banner.p'))
 print data
+
+for this in data:
+    print "".join(i[0]*i[1] for i in this)
+    
