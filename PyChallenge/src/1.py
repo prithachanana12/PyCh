7c32951ca05f3b48b4ce97174572386079fa99aa
Pritha Chanana <prithachanana@gmail.com>
2015-04-09 09:41:35 -0700

    edit

--- PyChallenge/src/1.py
+++ PyChallenge/src/1.py
@@ -5,7 +5,8 @@ Created on Feb 22, 2015
 '''
 
 from string import maketrans
-mystring="g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+#mystring="g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+mystring="map"
 trans=maketrans('abcdefghijklmnopqrstuvwxyz','cdefghijklmnopqrstuvwxyzab')
 print mystring.translate(trans)
-    
+
